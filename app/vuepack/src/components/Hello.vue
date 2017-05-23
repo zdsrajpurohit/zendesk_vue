0f98fadc0ba47ac0391cc88ceacb6bce76c9413e
Shakti <shaktirp1@gmail.com>
2017-05-23 11:36:59 -0700

    first commit

---
+++ app/vuepack/src/components/Hello.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="hello">
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      userName: ''
+    }
+  },
+  computed: {
+    msg: function () {
+      return 'Hello ' + this.userName + ', welcome to Zendesk Vue.js App.'
+    }
+  },
+  mounted: function () {
+    client.get('currentUser').then(function (data) {
+      this.userName = data['currentUser']['name']
+    }.bind(this))
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+p {
+  color: #bd322c;
+}
+</style>
