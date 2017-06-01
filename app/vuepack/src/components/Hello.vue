21835e0636d30ce33972a500145fb3a5c07834b7
Shakti <shaktirp1@gmail.com>
2017-05-31 20:06:50 -0700

    zendesk vue

--- app/vuepack/src/components/Hello.vue
+++ app/vuepack/src/components/Hello.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="hello">
     <p>{{ msg }}</p>
+    <ul>
+      <li v-for="consultant in consultants">
+        {{ consultant['DC Name'] }} - {{ consultant['Store'] }}
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -9,7 +14,9 @@ export default {
   name: 'hello',
   data () {
     return {
-      userName: ''
+      userName: '',
+      consultants: [],
+      ROWS: ['Store', 'Lead Allocation', 'DC Name', 'Region', 'Capacity', 'Store No', 'Store PC', 'Store Tele', 'DC Email']
     }
   },
   computed: {
@@ -17,10 +24,41 @@ export default {
       return 'Hello ' + this.userName + ', welcome to Zendesk Vue.js App.'
     }
   },
+  methods: {
+    getGoogleSheet: function () {
+      var sheetId = '1xBy31E2YCfQpH_qsB-Mwy3oqfT_PvAdD3SN3eb_ddKk'
+      var key = 'AIzaSyBQSvfLyFNJCWfIPOUBnZxi-wl-Hn3le5s'
+      var range = 'A1:ZZ10000'
+      var url = 'https://sheets.googleapis.com/v4/spreadsheets/' + sheetId + '/values/' + range + '?key=' + key
+      var app = this
+
+      client.request({
+        url: url
+      }).then(function (data) {
+        // Remove header rows
+        data['values'] = data['values'].slice(5)
+
+        // Convert array of rows to an object
+        app.consultants = data['values'].map(function (row) {
+          var rowObj = {}
+          app.ROWS.forEach(function (header, i) {
+            rowObj[header] = row[i]
+          })
+          return rowObj
+        })
+
+        console.log(app.consultants)
+      },
+      function (response) {
+        console.error(response)
+      })
+    }
+  },
   mounted: function () {
     client.get('currentUser').then(function (data) {
       this.userName = data['currentUser']['name']
     }.bind(this))
+    this.getGoogleSheet()
   }
 }
 </script>
@@ -29,5 +67,9 @@ export default {
 <style scoped>
 p {
   color: #bd322c;
+}
+ul {
+  list-style: none;
+  padding-left: 0;
 }
 </style>
